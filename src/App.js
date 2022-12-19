ea2e658ffeeeec4973d7cdc301e51d84ea34c6c7
ar-rony <ar.rony87@gmail.com>
2022-12-20 00:10:39 +0600

    Heading Changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>My React App Working</h1>
+        <h1>Aurno React App</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
